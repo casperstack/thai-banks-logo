340d44376bcd5d0ce4599b3d6b6f7b066f0c18d2
fasterv410 <fasterv_410@hotmail.com>
2023-09-08 14:32:04 +0700

    Update bank lists position

--- index.ts
+++ index.ts
@@ -1,12 +1,33 @@
 import { BankList } from './types'
 
 const bankLists: BankList = {
-  BAAC: {
-    name: "ธ.ก.ส.",
-    nameLong: "ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร",
-    nameEN: "Bank for Agriculture and Agricultural Cooperatives",
-    symbol: "BAAC",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/BAAC.png"
+  KBANK: {
+    name: "กสิกรไทย",
+    nameLong: "ธนาคารกสิกรไทย",
+    nameEN: "Kasikorn Bank",
+    symbol: "KBANK",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KBANK.png"
+  },
+  SCB: {
+    name: "ไทยพาณิชย์",
+    nameLong: "ธนาคารไทยพาณิชย์",
+    nameEN: "The Siam Commercial Bank",
+    symbol: "SCB",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/SCB.png"
+  },
+  KTB: {
+    name: "กรุงไทย",
+    nameLong: "ธนาคารกรุงไทย",
+    nameEN: "Krungthai Bank",
+    symbol: "KTB",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KTB.png"
+  },
+  BBL: {
+    name: "กรุงเทพ",
+    nameLong: "ธนาคารกรุงเทพ",
+    nameEN: "Bangkok Bank",
+    symbol: "BBL",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/BBL.png"
   },
   BAY: {
     name: "กรุงศรีอยุธยา",
@@ -15,12 +36,40 @@ const bankLists: BankList = {
     symbol: "BAY",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/BAY.png"
   },
-  BBL: {
-    name: "กรุงเทพ",
-    nameLong: "ธนาคารกรุงเทพ",
-    nameEN: "Bangkok Bank",
-    symbol: "BBL",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/BBL.png"
+  TTB: {
+    name: "ทีเอ็มบีธนชาต",
+    nameLong: "ธนาคารทีเอ็มบีธนชาต",
+    nameEN: "TMBThanachart Bank",
+    symbol: "TTB",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/TTB.png"
+  },
+  UOB: {
+    name: "ยูโอบี",
+    nameLong: "ธนาคารยูโอบี",
+    nameEN: "United Overseas Bank",
+    symbol: "UOB",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/UOB.png"
+  },
+  KKP: {
+    name: "เกียรตินาคิน",
+    nameLong: "ธนาคารเกียรตินาคินภัทร",
+    nameEN: "Kiatnakin Phatra Bank",
+    symbol: "KKP",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KKP.png"
+  },
+  GSB: {
+    name: "ออมสิน",
+    nameLong: "ธนาคารออมสิน",
+    nameEN: "Government Savings Bank",
+    symbol: "GSB",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/GSB.png"
+  },
+  BAAC: {
+    name: "ธ.ก.ส.",
+    nameLong: "ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร",
+    nameEN: "Bank for Agriculture and Agricultural Cooperatives",
+    symbol: "BAAC",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/BAAC.png"
   },
   CIMB: {
     name: "ซีไอเอ็มบี",
@@ -43,13 +92,6 @@ const bankLists: BankList = {
     symbol: "GHB",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/GHB.png"
   },
-  GSB: {
-    name: "ออมสิน",
-    nameLong: "ธนาคารออมสิน",
-    nameEN: "Government Savings Bank",
-    symbol: "GSB",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/GSB.png"
-  },
   HSBC: {
     name: "เอชเอสบีซี",
     nameLong: "ธนาคารเอชเอสบีซี",
@@ -71,47 +113,12 @@ const bankLists: BankList = {
     symbol: "ICBC",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/ICBC.png"
   },
-  KBANK: {
-    name: "กสิกรไทย",
-    nameLong: "ธนาคารกสิกรไทย",
-    nameEN: "Kasikorn Bank",
-    symbol: "KBANK",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KBANK.png"
-  },
-  KKP: {
-    name: "เกียรตินาคิน",
-    nameLong: "ธนาคารเกียรตินาคินภัทร",
-    nameEN: "Kiatnakin Phatra Bank",
-    symbol: "KKP",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KKP.png"
-  },
-  KTB: {
-    name: "กรุงไทย",
-    nameLong: "ธนาคารกรุงไทย",
-    nameEN: "Krungthai Bank",
-    symbol: "KTB",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/KTB.png"
-  },
   LHB: {
     name: "แลนด์ แอนด์ เฮ้าส์",
     nameLong: "ธนาคารแลนด์ แอนด์ เฮ้าส์",
     nameEN: "LH Bank",
     symbol: "LHB",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/LHB.png"
-  },
-  PromptPay: {
-    name: "พร้อมเพย์",
-    nameLong: "พร้อมเพย์",
-    nameEN: "PromptPay",
-    symbol: "PromptPay",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/PromptPay.png"
-  },
-  SCB: {
-    name: "ไทยพาณิชย์",
-    nameLong: "ธนาคารไทยพาณิชย์",
-    nameEN: "The Siam Commercial Bank",
-    symbol: "SCB",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/SCB.png"
   },
   TCRB: {
     name: "ไทยเครดิต",
@@ -127,26 +134,19 @@ const bankLists: BankList = {
     symbol: "TISCO",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/TISCO.png"
   },
+  PromptPay: {
+    name: "พร้อมเพย์",
+    nameLong: "พร้อมเพย์",
+    nameEN: "PromptPay",
+    symbol: "PromptPay",
+    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/PromptPay.png"
+  },
   TrueMoney: {
     name: "ทรูมันนี่",
     nameLong: "ทรูมันนี่",
     nameEN: "True Money",
     symbol: "TrueMoney",
     icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/TrueMoney.png"
-  },
-  TTB: {
-    name: "ทีเอ็มบีธนชาต",
-    nameLong: "ธนาคารทีเอ็มบีธนชาต",
-    nameEN: "TMBThanachart Bank",
-    symbol: "TTB",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/TTB.png"
-  },
-  UOB: {
-    name: "ยูโอบี",
-    nameLong: "ธนาคารยูโอบี",
-    nameEN: "United Overseas Bank",
-    symbol: "UOB",
-    icon: "https://raw.githubusercontent.com/casperstack/thai-banks-logo/master/icons/UOB.png"
   }
 }
 
